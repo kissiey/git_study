f0d768f6ec52ef60e20975b92e09277f53d3cc97
kissiey <kissiey@hotmail.com>
2016-09-12 22:35:02 +0800

    add test.c

---
+++ test.c
@@ -0,0 +1,20 @@
+/* ************************************************************************
+ *       Filename:  test.c
+ *    Description:  
+ *        Version:  1.0
+ *        Created:  2016年09月12日 22时32分24秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *         Author:  YOUR NAME (), 
+ *        Company:  
+ * ************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc , char **argv)
+{
+	printf("hello, world1\n");
+	return 0;
+}
